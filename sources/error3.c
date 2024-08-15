a91b67f77c0419ed32b6259b0291f513dabbf4cd
thbasse <thbasse@student.42.fr>
2024-08-15 10:25:21 +0200

    update

--- sources/error3.c
+++ sources/error3.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 18:38:21 by thbasse           #+#    #+#             */
-/*   Updated: 2024/08/15 09:59:55 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/08/15 10:16:05 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int check_wall(t_map *map)
 {
 	int error_code;
 
-	error_code = 0;
 	error_code = check_boder(map);
-	if (error_code == 0)
-		error_code =check_side(map);
+	if (error_code != 0)
+		return error_code;
+	error_code = check_side(map);
 	return (error_code);
 }
 
@@ -27,41 +27,37 @@ int	check_boder(t_map *map)
 {
 	int h;
 	int w;
-	int	error_code;
 
-	error_code = 0;
 	w = 0;
 	while (map->content[0][w] && map->content[0][w] != '\n')
 	{
 		if (map->content[0][w] != '1')
-			error_code = 413;
+			return (413);
 		w++;
 	}
-	w = 0;
 	h = 0;
 	while(map->content[h])
 		h++;
+	w = 0;
 	while (map->content[h - 1][w] && map->content[h - 1][w] != '\n')
 	{
 		if (map->content[h - 1][w] != '1')
-			error_code = 413;
+			return (413);
 		w++;
 	}
-	return(error_code);
+	return (0);
 }
 
 int	check_side(t_map *map)
 {
 	int h;
 	int w;
-	int	error_code;
 
-	error_code = 0;
 	h = 0;
 	while (map->content[h])
 	{
 		if (map->content[h][0] != '1')
-			error_code = 413;
+			return (413);
 		h++;
 	}
 	h = 0;
@@ -69,8 +65,8 @@ int	check_side(t_map *map)
 	while (map->content[h])
 	{
 		if (map->content[h][w] != '1')
-			error_code = 413;
+			return (413);
 		h++;
 	}
-	return(error_code);
+	return (0);
 }
