7225a31ddf0439d81fd5a85c8e9184abd456cff8
thbasse <thbasse@student.42.fr>
2024-08-29 13:30:19 +0200

    update

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:27:05 by thbasse           #+#    #+#             */
-/*   Updated: 2024/08/21 10:13:04 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/08/29 13:28:38 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ int	main(int argc, char **argv)
 	error_code = check_error(argc, argv, map);
 	if (error_code > 406)
 		free_tab(map->content);
-	start_game(map);
-	free_tab(map->content);
+	if (error_code == 0)
+	{
+		start_game(map);
+		free_tab(map->content);
+	}
 	free(map);
 }
